aed3ca32e7dd064897f1f8649880a533d4036d4c
blueapple99 <blueapple99@naver.com>
2024-01-17 18:18:36 +0900

    feat : FishAdoption 모달창 수정
    - 기존 정적인 가격에서 flow에 따라 동적인 가격이 들어가게끔 수정
    - figma에 맞게 스타일 수정

--- src/components/resources/category/container/FishAdoption.tsx
+++ src/components/resources/category/container/FishAdoption.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
 import * as s from '../styles/FishAdoptionStyle';
+import { FishData } from '../../../../assets/data/fishData';
+
+const price_array = FishData.items.map(item=> item.salePrice);
 
 const FishAdoption = ({ data, payload, close }: any) => {
+  const currentPathname = window.location.pathname;
+  const idx = Number(currentPathname.slice(-1))-1;
+  const price = price_array[idx];
   const [quantity, setQuantity] = useState(1);
+  const [payment, setPayment] = useState(price);
 
   return (
     <s.FishAdoptionContainer>
       <s.FishAdoptionHeader>
-        <s.FishAdoptionPrice>1,000,000원</s.FishAdoptionPrice>
+        <s.FishAdoptionPrice>{payment.toLocaleString()+'원'}</s.FishAdoptionPrice>
       </s.FishAdoptionHeader>
       <s.FishAdoptionBody>
         <s.FishAdoptionQuantity>수량</s.FishAdoptionQuantity>
@@ -16,6 +23,7 @@ const FishAdoption = ({ data, payload, close }: any) => {
             onClick={() => {
               if (quantity > 1) {
                 setQuantity(quantity - 1);
+                setPayment(payment-price);
               }
             }}
           >
@@ -25,6 +33,7 @@ const FishAdoption = ({ data, payload, close }: any) => {
           <s.FishAdoptionQuantitySelectorButton
             onClick={() => {
               setQuantity(quantity + 1);
+              setPayment(payment+price);
             }}
           >
             +
